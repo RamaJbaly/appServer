734eaed85ffbfdf21b03ca578d869ccbc78b3b18
RamaJbaly <ramajbaly25@gmail.com>
2024-12-28 04:40:18 -0800

    ...

--- API/routes/user.routers.js
+++ API/routes/user.routers.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const userRouter = express.Router();
-const { createUser, FindUser, DeletUser, updateuser } = require("../controllers/user.controllers");
+const { createUser, FindUser, DeletUser, updateuser, Login } = require("../controllers/user.controllers");
 
 userRouter.post("/createUser", createUser);
 
 userRouter.post("/FindUser", FindUser)
 userRouter.delete("/DeletUser", DeletUser)
 userRouter.patch("/ updateuser",  updateuser)
+userRouter.patch("/login",  Login)
+
 
 module.exports = userRouter;
