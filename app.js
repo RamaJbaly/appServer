8dcbdb9ef3e33314ae0773c42d003a2b13e5de5d
RamaJbaly <ramajbaly25@gmail.com>
2025-01-04 04:12:01 -0800

    ///

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ app.use(express.json());
 // app.use(cors());
 // app.use('/' , Routes)
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
 const mongooseURL = "mongodb+srv://Rama_server:123456780@cluster0.snxtn.mongodb.net/"
 mongoose.connect(mongooseURL);
 
